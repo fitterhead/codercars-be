f26878068b538eeb6d7ca24c31774610c9e96e47
phi <xuanphiart@gmail.com>
2022-11-07 12:27:33 -0500

    new commit

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,17 +1,32 @@
+
 const { sendResponse, AppError } = require("../helpers/utils.js");
 const mongoose = require("mongoose");
 const Car = require("../models/Car");
-
 const carController = {};
+const allowUpdate = [
+  "make",
+  "model",
+  "release_date",
+  "transmission_type",
+  "price",
+  "size",
+  "style",
+];
 /* -------------------------------------------------------------------------- */
 /*                                Create a car                                */
 /* -------------------------------------------------------------------------- */
-
 carController.createCar = async (req, res, next) => {
   try {
-    const info = req.body;
-    if (!info) throw new AppError(402, "Bad Request", "Create car Error");
-    const created = await Car.create(info);
+    const updates = req.body;
+    if (!updates) throw new AppError(402, "Bad Request", "Create car Error");
+    const updateKeys = Object.keys(updates);
+    const notAllow = updateKeys.filter((el) => !allowUpdate.includes(el));
+    if (notAllow.length) {
+      const exception = new Error(`Update field not allow`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+    const created = await Car.create(updates);
     sendResponse(
       res,
       200,
@@ -24,14 +39,17 @@ carController.createCar = async (req, res, next) => {
     next(err);
   }
 };
-
 /* -------------------------------------------------------------------------- */
 /*                                 Get all car                                */
 /* -------------------------------------------------------------------------- */
 carController.getCars = async (req, res, next) => {
+  const query = req.query;
+  console.log(query, "query");
   const filter = {};
   try {
-    const listOfFound = await Car.find(filter).limit(10);
+    const listOfFound = await Car.find(query)
+      .limit(10)
+      .skip((query.page - 1) * 10);
     sendResponse(
       res,
       200,
@@ -44,20 +62,26 @@ carController.getCars = async (req, res, next) => {
     next(err);
   }
 };
-
 /* -------------------------------------------------------------------------- */
 /*                                Update a car                                */
 /* -------------------------------------------------------------------------- */
 carController.editCar = async (req, res, next) => {
-  const targetId = req.params.id;
-  console.log(targetId);
-  const updateInfo = req.body;
-  //options allow you to modify query. e.g new true return lastest update of data
-  const options = { new: true };
   try {
+    const targetId = req.params.id;
+    console.log(targetId);
+    const updates = req.body;
+    if (!updates) throw new AppError(402, "Bad Request", "Create car Error");
+    const updateKeys = Object.keys(updates);
+    //find update request that not allow
+    const notAllow = updateKeys.filter((el) => !allowUpdate.includes(el));
+    if (notAllow.length) {
+      const exception = new Error(`Update field not allow`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+    const options = { new: true };
     //mongoose query
-    const updated = await Car.findByIdAndUpdate(targetId, updateInfo, options);
-
+    const updated = await Car.findByIdAndUpdate(targetId, updates, options);
     sendResponse(
       res,
       200,
@@ -70,7 +94,6 @@ carController.editCar = async (req, res, next) => {
     next(err);
   }
 };
-
 /* -------------------------------------------------------------------------- */
 /*                                 Delete Car                                 */
 /* -------------------------------------------------------------------------- */
@@ -80,7 +103,6 @@ carController.deleteCar = async (req, res, next) => {
   try {
     //mongoose query
     const updated = await Car.findByIdAndDelete(targetId, options);
-
     sendResponse(
       res,
       200,
@@ -93,5 +115,4 @@ carController.deleteCar = async (req, res, next) => {
     next(err);
   }
 };
-
 module.exports = carController;
--- models/Car.js
+++ models/Car.js
@@ -1,11 +1,43 @@
-
+// {
+//   "make": "BMW",
+//   "model": "haha",
+//   "release_date": 1999,
+//   "transmission_type": "MANUAL",
+//   "price": 3232,
+//   "size": "Compact",
+//   "style": "xsdsdsda"
+// }
 const mongoose = require("mongoose");
 const carSchema = new mongoose.Schema({
+  // isDeleted: { type: Boolean, defaults: false },
   make: {
     type: String,
     required: true,
   },
-  
+  model: {
+    type: String,
+    required: true,
+  },
+  release_date: {
+    type: Number,
+    required: true,
+  },
+  transmission_type: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  size: {
+    type: String,
+    required: true,
+  },
+  style: {
+    type: String,
+    required: true,
+  },
 });
 
 carSchema.pre(/^find/, function (next) {
